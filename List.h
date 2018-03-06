9a9599bbcc7664917f7a747f5f2f7997ffaa4d6b
violet6672 <violet6672@users.noreply.github.com>
2018-03-06 11:33:24 -0400

    List.h

--- List.h
+++ List.h
@@ -18,7 +18,6 @@ class List //begin List definition
      void insert(int element, int k);//insert element at location k
      void remove(int k);//remove element at location k
      int size();//return the number of elements in the List
-     void clear;
      Node* get(int k);
 
      
