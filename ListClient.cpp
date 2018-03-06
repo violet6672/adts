a6510a76dc11338f6e6d6189bbb87256055fea1b
violet6672 <violet6672@users.noreply.github.com>
2018-03-06 11:42:43 -0400

    ListClient.cpp

--- ListClient.cpp
+++ ListClient.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 int main()
 {
-
- List L1, L2;
+	List L1, L2;
 
  //Do some stuff with L1, L2, ...
  // ...
+
+	L1.insert(12,1);
+	L1.insert(15,2);
+	L2.insert(13,1);
+	L2.insert(16,2);
+	L1.get(2);
+	L2.get(1);
 
 }
